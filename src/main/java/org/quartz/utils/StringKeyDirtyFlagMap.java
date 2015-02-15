d465069d6b64ef1733bb289fd25ef3087d31bb80
Tim Molter <tim.molter@gmail.com>
2015-02-15 18:23:33 +0100

    removed unused code

--- src/main/java/org/quartz/utils/StringKeyDirtyFlagMap.java
+++ src/main/java/org/quartz/utils/StringKeyDirtyFlagMap.java
@@ -1,26 +1,24 @@
-/* 
- * Copyright 2001-2009 Terracotta, Inc. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * Copyright 2001-2009 Terracotta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
  */
 package org.quartz.utils;
 
-import java.util.Iterator;
-import java.util.Map;
-
 /**
  * <p>
- * An implementation of <code>Map</code> that wraps another <code>Map</code> and flags itself 'dirty' when it is modified, enforces that all keys are Strings.
+ * An implementation of <code>Map</code> that wraps another <code>Map</code> and flags itself 'dirty' when it is modified, enforces that all keys are
+ * Strings.
  * </p>
  * <p>
  * All allowsTransientData flag related methods are deprecated as of version 1.6.
@@ -41,52 +39,9 @@ public class StringKeyDirtyFlagMap extends DirtyFlagMap {
   }
 
   @Override
-  public boolean equals(Object obj) {
-
-    return super.equals(obj);
-  }
-
-  @Override
   public int hashCode() {
 
     return getWrappedMap().hashCode();
-  }
-
-  /**
-   * Get a copy of the Map's String keys in an array of Strings.
-   */
-  public String[] getKeys() {
-
-    return (String[]) keySet().toArray(new String[size()]);
-  }
-
-  /**
-   * <p>
-   * Adds the name-value pairs in the given <code>Map</code> to the <code>StringKeyDirtyFlagMap</code>.
-   * </p>
-   * <p>
-   * All keys must be <code>String</code>s.
-   * </p>
-   */
-  @Override
-  public void putAll(Map map) {
-
-    for (Iterator entryIter = map.entrySet().iterator(); entryIter.hasNext();) {
-      Map.Entry entry = (Map.Entry) entryIter.next();
-
-      // will throw IllegalArgumentException if key is not a String
-      put(entry.getKey(), entry.getValue());
-    }
-  }
-
-  /**
-   * <p>
-   * Adds the given <code>String</code> value to the <code>StringKeyDirtyFlagMap</code>.
-   * </p>
-   */
-  public void put(String key, String value) {
-
-    super.put(key, value);
   }
 
   /**
@@ -108,7 +63,7 @@ public class StringKeyDirtyFlagMap extends DirtyFlagMap {
    * <p>
    * Retrieve the identified <code>String</code> value from the <code>StringKeyDirtyFlagMap</code>.
    * </p>
-   * 
+   *
    * @throws ClassCastException if the identified object is not a String.
    */
   public String getString(String key) {
