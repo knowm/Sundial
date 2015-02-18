76d85a796e224690799a883071b7ed2c31366a59
Tim Molter <tim.molter@gmail.com>
2015-02-18 21:58:47 +0100

    misc tweaks

--- src/main/java/org/quartz/triggers/CronTriggerImpl.java
+++ src/main/java/org/quartz/triggers/CronTriggerImpl.java
@@ -444,30 +444,4 @@ public class CronTriggerImpl extends AbstractTrigger implements CronTrigger {
     return (cronEx == null) ? null : cronEx.getTimeBefore(endTime);
   }
 
-  //  /**
-  //   * Get a {@link ScheduleBuilder} that is configured to produce a schedule identical to this trigger's schedule.
-  //   *
-  //   * @see #getTriggerBuilder()
-  //   */
-  //  @Override
-  //  public ScheduleBuilder getScheduleBuilder() {
-  //
-  //    CronScheduleBuilder cb = null;
-  //    try {
-  //      cb = CronScheduleBuilder.cronSchedule(getCronExpression()).inTimeZone(getTimeZone());
-  //    } catch (ParseException ignore) {
-  //      // can't happen (because the expression was validated to get here in the first place)
-  //    }
-  //
-  //    switch (getMisfireInstruction()) {
-  //    case MISFIRE_INSTRUCTION_DO_NOTHING:
-  //      cb.withMisfireHandlingInstructionDoNothing();
-  //      break;
-  //    case MISFIRE_INSTRUCTION_FIRE_ONCE_NOW:
-  //      cb.withMisfireHandlingInstructionFireAndProceed();
-  //      break;
-  //    }
-  //
-  //    return cb;
-  //  }
 }
